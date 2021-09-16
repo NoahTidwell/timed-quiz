827785ccd13c7692155e33e9a838afe67dd03735
Noah Tidwell <ntidwell34@yahoo.com>
2021-09-15 20:02:30 -0500

    initial set up, set up javascript questions up

---
+++ script.js
@@ -0,0 +1,28 @@
+var quiz = [
+    {
+    questions: "Question 1",
+    options: "Answer Choices",
+    answer: "correct answer"
+    },
+    {
+    questions: "Question 2",
+    options: "Answer Choices",
+    answer: "correct answer"
+    },
+    {
+    questions: "Question 3",
+    options: "Answer Choices",
+    answer: "correct answer"
+    },
+    {
+    questions: "Question 4",
+    options: "Answer Choices",
+    answer: "correct answer"
+    },
+    {
+    questions: "Question 5",
+    options: "Answer Choices",
+    answer: "correct answer"
+    },
+
+];
