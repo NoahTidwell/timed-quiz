4f8425f89da83a59f75df9109e3c0d08d6241a8e
Noah Tidwell <ntidwell34@yahoo.com>
2021-09-15 20:32:14 -0500

    update questions, initial CSS

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-var quiz = [
+// Quiz questions Array
+var quizQuestions = [
     {
-    questions: "Question 1",
-    options: "Answer Choices",
-    answer: "correct answer"
+    questions: "Commonly used data types DO NOT include:",
+    options: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alets"
     },
     {
-    questions: "Question 2",
-    options: "Answer Choices",
-    answer: "correct answer"
+    questions: "The condition in an if / else statement is enclosed within ____.",
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "curly brackets"
     },
     {
-    questions: "Question 3",
-    options: "Answer Choices",
-    answer: "correct answer"
+    questions: "Arrays in Javascript can be used to store ____.",
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
     },
     {
-    questions: "Question 4",
-    options: "Answer Choices",
-    answer: "correct answer"
+    questions: "String values must be enclosed within ____ when being assigned to variables.",
+    options: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
     },
     {
-    questions: "Question 5",
-    options: "Answer Choices",
-    answer: "correct answer"
+    questions: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    options: ["javascript", "terminal/bash", "for loops", "console log"],
+    answer: "console log"
     },
 
 ];
+
+// Define Initial Variables
+var score = 0;
+var questionIndex = 0;
+var quizTime = 90;
+
