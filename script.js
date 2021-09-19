a9ddf5965c8683b428be85531cd56869d99ebaea
Noah Tidwell <ntidwell34@yahoo.com>
2021-09-19 11:45:32 -0500

    fix broken logic since quizQuestions Array changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let quizQuestions = [
     {
     question: "Commonly used data types DO NOT include:",
-    options: [
+    option: [
         {text: "strings", correct: false},
         {text: "booleans", correct: false},
         {text: "alerts", correct: true},
@@ -11,7 +11,7 @@ let quizQuestions = [
 },
 {
     question: "The condition in an if / else statement is enclosed within ____.",
-    options: [
+    option: [
         {text: "quotes", correct: false},
         {text: "curly brackets", correct: true},
         {text: "parentheses", correct: false},
@@ -20,7 +20,7 @@ let quizQuestions = [
     },
     {
     question: "Arrays in Javascript can be used to store ____.",
-    options: [
+    option: [
         {text: "numbers & strings", correct: false},
         {text: "other arrays", correct: false},
         {text: "booleans", correct: false},
@@ -29,7 +29,7 @@ let quizQuestions = [
     },
     {
     question: "String values must be enclosed within ____ when being assigned to variables.",
-    options: [
+    option: [
         {text: "commas", correct: false},
         {text: "curly brackets", correct: false},
         {text: "quotes", correct: true},
@@ -38,14 +38,15 @@ let quizQuestions = [
     },
     {
     question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-    options: [
+    option: [
         {text: "javascript", correct: false},
         {text: "terminal/bash", correct: false},
         {text: "for loops", correct: false},
         {text: "console log", correct: true}
     ]
+}
 
-];
+]
 
 // Define Initial Variables
 let counter = 0;
@@ -57,7 +58,7 @@ let acceptingAnswers = false;
 
 // In Page Objects
 let timer = document.querySelector("#timer");
-let quizOptions = document.querySelector("#question-container");
+let questionContainerElement = document.querySelector("#question-container");
 let quizStart = document.querySelector("#start-quiz");
 let nextQuestion = document.querySelector("next-btn");
 let mainPage = document.querySelector("#main-page");
@@ -66,7 +67,7 @@ let answer1 = document.querySelector("#btn-1");
 let answer2 = document.querySelector("#btn-2");
 let answer3 = document.querySelector("#btn-3");
 let answer4 = document.querySelector("#btn-4");
-let questionEl = document.getElementById("question-box");
+let quizEl = document.getElementById("question-box");
 let answerButtonElement = document.getElementById("answer-buttons");
 
 // Event listener on Start Quiz Button
@@ -97,25 +98,25 @@ var setNextQuestion = function() {
 }
 
 var showQuestions = function(quizQuestions) {
-    reset();
+    clear();
 // Clear Original Quiz Box
-    questionEl.innerHTML = quizQuestions.questions;
+    quizEl.innerText = quizQuestions.question;
 // Create New buttons and append to the page
-    quizQuestions.options.forEach(options => {
+    quizQuestions.option.forEach(option => {
         const button = document.createElement("button")
-        button.innerText = options;
+        button.innerText = option.text;
         button.classList.add("btn")
         button.classList.add("btn-primary")
-        if (options.answer) {
-            button.dataset.answer = options.answer
+        if (option.correct) {
+            button.dataset.correct = option.correct
         }
         button.addEventListener("click", selectAnswer)
         answerButtonElement.append(button)
     })
    
 }
-// Reset Page to prep for the Quiz
-var reset = function () {
+// Clear Page to prep for the Quiz
+var clear = function () {
     quizStart.classList.add("hide")
     while (answerButtonElement.firstChild) {
         answerButtonElement.removeChild(answerButtonElement.firstChild)
@@ -123,13 +124,24 @@ var reset = function () {
 } 
 var selectAnswer = function(event) {
     var answerChoice = event.target
-    console.log(answerChoice)
+    const correct = answerChoice.dataset.correct 
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonElement.children).forEach(button => {
+        setStatusClass(button.dataset.correct)
+    })
 }
 
-var setStatusClass = function() {
-    
+var setStatusClass = function(element, correct) {
+    clearStatusClass(element)
+    if(correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
     
 }
 
-var clearStatusClass = function() {
+var clearStatusClass = function(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong");
 }
